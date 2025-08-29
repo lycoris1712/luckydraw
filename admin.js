656ed72c5e4238592ad1598e5b533161527360a8
lycoris1712 <121221572+lycoris1712@users.noreply.github.com>
2025-08-29 13:40:02 +0800

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,27 +1,29 @@
 import { db, collection, getDocs } from './admin.html';
 
 const participantsDiv = document.getElementById("participants");
-const winnerP = document.getElementById("winner");
+const countSpan = document.getElementById("count");
 const spinBtn = document.getElementById("spin");
+const winnerP = document.getElementById("winner");
 
-// Show all participants
-async function showParticipants() {
-  participantsDiv.innerHTML = "";
+async function getParticipants() {
   const snapshot = await getDocs(collection(db, "participants"));
   const participants = snapshot.docs.map(doc => doc.data());
+  participantsDiv.innerHTML = "";
   participants.forEach(p => {
     const pEl = document.createElement("p");
     pEl.innerText = `${p.name} - ${p.number}`;
     participantsDiv.appendChild(pEl);
   });
+  countSpan.innerText = participants.length;
   return participants;
 }
 
 // Spin the wheel
 spinBtn.onclick = async () => {
-  const participants = await showParticipants();
+  const participants = await getParticipants();
   if(participants.length === 0) return alert("No participants yet!");
   const winner = participants[Math.floor(Math.random() * participants.length)];
   winnerP.innerText = `${winner.name} - ${winner.number}`;
 };
-showParticipants();
+
+getParticipants();
